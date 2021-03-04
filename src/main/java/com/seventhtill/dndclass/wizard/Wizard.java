f3e735c5535390c6133d7aceba66f887666cf493
Dylan King <kingdylan11@outlook.com>
2021-03-04 13:36:46 +0000

    Added Magical interface to Wizard class.

--- src/main/java/com/seventhtill/dndclass/wizard/Wizard.java
+++ src/main/java/com/seventhtill/dndclass/wizard/Wizard.java
@@ -1,6 +1,7 @@
 package com.seventhtill.dndclass.wizard;
 
 import com.seventhtill.dndclass.DnDClass;
+import com.seventhtill.magic.Magical;
 
-public class Wizard implements DnDClass {
+public class Wizard implements DnDClass, Magical {
 }
