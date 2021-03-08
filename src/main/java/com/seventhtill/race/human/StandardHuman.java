13f0e3b4d69261bfb9fe340f339e3f92098424da
Shwingbad <38210042+Shwingbad@users.noreply.github.com>
2021-03-08 17:13:28 +0000

    Created base human structure

--- src/main/java/com/seventhtill/race/human/StandardHuman.java
+++ src/main/java/com/seventhtill/race/human/StandardHuman.java
@@ -1,4 +1,9 @@
 package com.seventhtill.race.human;
 
+// Concrete standard human class that extends base human
 public class StandardHuman extends Human {
+    // Constructor
+    public StandardHuman() {
+        super();
+    }
 }
