4733287ee73fa3eb3217063a4550245689b961cc
Shwingbad <38210042+Shwingbad@users.noreply.github.com>
2021-03-08 16:04:05 +0000

    Created base dwarf structure

--- src/main/java/com/seventhtill/Main.java
+++ src/main/java/com/seventhtill/Main.java
@@ -1,7 +1,12 @@
 package com.seventhtill;
 
+import com.seventhtill.race.dwarf.Dwarf;
+import com.seventhtill.race.dwarf.HillDwarf;
+import com.seventhtill.race.dwarf.MountainDwarf;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Dwarf test = new HillDwarf(); //testing creation
     }
 }
--- src/main/java/com/seventhtill/race/dwarf/MountainDwarf.java
+++ src/main/java/com/seventhtill/race/dwarf/MountainDwarf.java
@@ -1,4 +1,23 @@
 package com.seventhtill.race.dwarf;
 
+import java.util.ArrayList;
+
+// Concrete class for Mountain Dwarf that extends Dwarf
 public class MountainDwarf extends Dwarf {
+    // Constructor
+    public MountainDwarf() {
+        super();
+        addAbilityScoreIncrease();
+        addAbility();
+    }
+
+    // Add Dwarven Armour Training ability
+    private void addAbility() {
+        this.abilities.add("Dwarven Armour Training");
+    }
+
+    // Adding 2 wisdom to the ability scores
+    private void addAbilityScoreIncrease() {
+        this.abilityScoreIncrease.put("Strength", 2);
+    }
 }
