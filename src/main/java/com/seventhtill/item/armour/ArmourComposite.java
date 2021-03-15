558bec31ce894e102753955db3c6e79c723c4eff
Dylan King <kingdylan11@outlook.com>
2021-03-15 12:44:51 +0000

    Refactored Base armour into composite instead.

---
+++ src/main/java/com/seventhtill/item/armour/ArmourComposite.java
@@ -0,0 +1,69 @@
+package com.seventhtill.item.armour;
+
+import java.util.ArrayList;
+
+public class ArmourComposite implements Armour {
+
+    private ArrayList<Armour> armourCollection;
+
+    public ArmourComposite() {
+        armourCollection = new ArrayList<>();
+    }
+
+    public ArmourComposite(ArrayList<Armour> armourCollection) {
+        this.armourCollection = armourCollection;
+    }
+
+
+    @Override
+    public int getBaseArmour() {
+        int totalBaseArmour = 0;
+
+        for (Armour armour : armourCollection) {
+            totalBaseArmour += armour.getBaseArmour();
+        }
+
+        return totalBaseArmour;
+    }
+
+    @Override
+    public int getArmourClassModifier(int dexModifier) {
+        int totalArmour = 0;
+
+        for (Armour armour : armourCollection) {
+            totalArmour += armour.getArmourClassModifier(dexModifier);
+        }
+
+        return totalArmour;
+    }
+
+    @Override
+    public boolean isDisadvantage() {
+        boolean result = false;
+
+        for (Armour armour : armourCollection) {
+            result |= armour.isDisadvantage();
+        }
+
+        return result;
+    }
+
+    @Override
+    public int getWeight() {
+        int totalWeight = 0;
+
+        for (Armour armour : armourCollection) {
+            totalWeight += armour.getWeight();
+        }
+
+        return totalWeight;
+    }
+
+    public void add(Armour armour) {
+        armourCollection.add(armour);
+    }
+
+    public void remove(Armour armour) {
+        armourCollection.remove(armour);
+    }
+}
---
+++ src/main/java/com/seventhtill/item/armour/Shield.java
@@ -0,0 +1,37 @@
+package com.seventhtill.item.armour;
+
+public class Shield implements Armour {
+    private int baseArmour;
+    private String name;
+    private int weight;
+
+    public Shield() {
+        this.baseArmour = 2;
+        this.name = "Shield";
+        this.weight = 6;
+    }
+
+    @Override
+    public int getBaseArmour() {
+        return baseArmour;
+    }
+
+    @Override
+    public int getArmourClassModifier(int dexModifier) {
+        return getBaseArmour();
+    }
+
+    @Override
+    public boolean isDisadvantage() {
+        return false;
+    }
+
+    @Override
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
