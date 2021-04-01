d7057f3b2e2cf9f71b0e14f47cb832f987642a88
Dylan King <kingdylan11@outlook.com>
2021-04-01 15:54:48 +0100

    Weapon properties is no longer final.

--- src/main/java/com/seventhtill/item/weapon/Weapon.java
+++ src/main/java/com/seventhtill/item/weapon/Weapon.java
@@ -10,7 +10,7 @@ public abstract class Weapon implements Item {
     protected WeaponAttackType attackType;
     private int weight;
     private String name;
-    private final List<String> properties;
+    private List<String> properties;
 
     public Weapon(WeaponAttackType attackType, int weight, String name, List<String> properties) {
         this.attackType = attackType;
