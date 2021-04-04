bed6190b15728baea6a43a371ce81770252e040a
Bmalone100 <36305546+Bmalone100@users.noreply.github.com>
2021-04-04 13:53:22 +0100

    >Removed the id inserts as they will be auto generated in db

--- src/main/java/com/seventhtill/dbManager/Queries.java
+++ src/main/java/com/seventhtill/dbManager/Queries.java
@@ -3,17 +3,16 @@ package com.seventhtill.dbManager;
 public class Queries {
 
     String addArmour(DnDCharacterDTO DnDCharacterDTO) {
-        String query = "INSERT INTO armour (id, name, disadvantage, baseArmour)" +
-                " VALUES " + DnDCharacterDTO.getArmourId() + DnDCharacterDTO.getArmourName()
+        String query = "INSERT INTO armour (name, disadvantage, baseArmour)" +
+                " VALUES " + DnDCharacterDTO.getArmourName()
                 + DnDCharacterDTO.isDisadvantage() + DnDCharacterDTO.getBaseArmour();
         return query;
     }
 
     String addWeapon(DnDCharacterDTO DnDCharacterDTO) {
-        String query = "INSERT INTO weapon (id, weight, properties, damageDie, damageType)" +
-                " VALUES " + DnDCharacterDTO.getWeaponId() + DnDCharacterDTO.getWeaponWeight()
-                + DnDCharacterDTO.getProperties() + DnDCharacterDTO.getDamageDie()
-                + DnDCharacterDTO.getDamageType();
+        String query = "INSERT INTO weapon (weight, properties, damageDie, damageType)" +
+                " VALUES " + DnDCharacterDTO.getWeaponWeight() + DnDCharacterDTO.getProperties()
+                + DnDCharacterDTO.getDamageDie() + DnDCharacterDTO.getDamageType();
         return query;
     }
 
@@ -25,7 +24,7 @@ public class Queries {
         return query;
     }
     String retrieveDnDCharacter() {
-        String query = "SELECT * FROM dndcharacter,armour,weapon" +
+        String query = "SELECT * FROM dndcharacter, armour, weapon" +
                 " WHERE characterArmour = armour.id" +
                 " OR characterWeapon = weapon.id";
         return query;
